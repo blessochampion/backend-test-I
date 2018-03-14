44e2509a39b421ab06d51ea7f7bca280a636e218
Blessing Ekundayo <blessochampion@gmail.com>
2018-03-14 19:23:21 +0100

    Change property variables to camelCase

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,8 @@ public class App {
     private static final String KEY_COLUMN_NAME = "column_name";
     private static final String KEY_COLUMN_FOLLOWERS_COUNT = "column_followers_count";
     private static final String KEY_QUEUE_COUNT= "queue_count";
+    private static final int TWITTER_FOLLOWER_LOWER_bOUND = 1000;
+    private static final int TWITTER_FOLLOWER_UPPER_bOUND = 5000;
 
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -26,29 +28,29 @@ public class App {
         String fileName = args[0];
         Properties prop = new Properties();
         InputStream inputStream;
-        String CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, SHEET_ID, COLUMN_NAME, COLUMN_FOLLOWERS_COUNT, queueCountStr;
-       int QUEUE_COUNT;
+        String consumerKey, consumerSecret, accessToken, accessTokenSecret, sheetId, columnName, columnFollowersCount, queueCountStr;
+       int queueCount;
 
         try {
             inputStream = new FileInputStream(fileName);
             prop.load(inputStream);
-            CONSUMER_KEY = prop.getProperty(KEY_CONSUMER_KEY);
-            CONSUMER_SECRET = prop.getProperty(KEY_CONSUMER_SECRET);
-            ACCESS_TOKEN = prop.getProperty(KEY_ACCESS_TOKEN);
-            ACCESS_TOKEN_SECRET = prop.getProperty(KEY_ACCESS_TOKEN_SECRET);
-            SHEET_ID = prop.getProperty(KEY_SHEET_ID);
-            COLUMN_NAME = prop.getProperty(KEY_COLUMN_NAME);
-            COLUMN_FOLLOWERS_COUNT = prop.getProperty(KEY_COLUMN_FOLLOWERS_COUNT);
+            consumerKey = prop.getProperty(KEY_CONSUMER_KEY);
+            consumerSecret = prop.getProperty(KEY_CONSUMER_SECRET);
+            accessToken = prop.getProperty(KEY_ACCESS_TOKEN);
+            accessTokenSecret = prop.getProperty(KEY_ACCESS_TOKEN_SECRET);
+            sheetId = prop.getProperty(KEY_SHEET_ID);
+            columnName = prop.getProperty(KEY_COLUMN_NAME);
+            columnFollowersCount = prop.getProperty(KEY_COLUMN_FOLLOWERS_COUNT);
                 queueCountStr = prop.getProperty(KEY_QUEUE_COUNT);
-            if (isNullOrEmpty(CONSUMER_KEY) || isNullOrEmpty(CONSUMER_SECRET)
-                    || isNullOrEmpty(ACCESS_TOKEN) || isNullOrEmpty(ACCESS_TOKEN_SECRET)
-            ||isNullOrEmpty(SHEET_ID) || isNullOrEmpty(COLUMN_NAME) || isNullOrEmpty(COLUMN_FOLLOWERS_COUNT)
+            if (isNullOrEmpty(consumerKey) || isNullOrEmpty(consumerSecret)
+                    || isNullOrEmpty(accessToken) || isNullOrEmpty(accessTokenSecret)
+            ||isNullOrEmpty(sheetId) || isNullOrEmpty(columnName) || isNullOrEmpty(columnFollowersCount)
                     || isNullOrEmpty(queueCountStr))
                     {
                 throw new PropertyException("Invalid property");
             }
-            QUEUE_COUNT = Integer.valueOf(queueCountStr);
-            if(QUEUE_COUNT <1 || QUEUE_COUNT > 10){
+            queueCount = Integer.valueOf(queueCountStr);
+            if(queueCount <1 || queueCount > 10){
                 throw new PropertyException(KEY_QUEUE_COUNT + " cannot be less that 1 and greater than 10");
             }
         } catch (FileNotFoundException e) {
@@ -71,15 +73,15 @@ public class App {
         }
 
         try {
-            GoogleSheetService.configureSheet(SHEET_ID, COLUMN_NAME, COLUMN_FOLLOWERS_COUNT, QUEUE_COUNT);
+            GoogleSheetService.configureSheet(sheetId, columnName, columnFollowersCount, queueCount);
         } catch (IOException e) {
             e.printStackTrace();
         }
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-        configurationBuilder.setOAuthConsumerKey(CONSUMER_KEY)
-                .setOAuthConsumerSecret(CONSUMER_SECRET)
-                .setOAuthAccessToken(ACCESS_TOKEN)
-                .setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
+        configurationBuilder.setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret);
         TwitterStream twitterStream = new TwitterStreamFactory(configurationBuilder.build()).getInstance();
         twitterStream.addListener(new StatusListener() {
             public void onException(Exception e) {
@@ -88,7 +90,7 @@ public class App {
 
             public void onStatus(Status status) {
                 User user = status.getUser();
-                if(1000 <= user.getFollowersCount() && user.getFollowersCount()<=5000){
+                if(TWITTER_FOLLOWER_LOWER_bOUND <= user.getFollowersCount() && user.getFollowersCount()<=TWITTER_FOLLOWER_UPPER_bOUND){
                     System.out.println(user.getScreenName() + " : " +user.getFollowersCount());
                     try {
                         GoogleSheetService.writeToGoogleSheet(user.getScreenName(), String.valueOf(user.getFollowersCount()));
